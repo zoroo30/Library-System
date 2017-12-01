27502e5d767a38b4fd9c4535728e62271d9aa165
nour9821 <33841295+nour9821@users.noreply.github.com>
2017-12-01 08:21:27 +0200

    Update books.h

--- books.h
+++ books.h
@@ -11,7 +11,7 @@ typedef struct
     char author[10];//same
     char publisher[10];//same
     char ISBN[20];
-    dateStruct* date;
+    dateStruct date;
     int copies;
     int current;
 } book;
