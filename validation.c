9adbc7ee13c7cdab262bdbdc7a54545dd7ae8ff0
AnwarRezk <33953443+AnwarRezk@users.noreply.github.com>
2017-12-01 18:25:34 +0200

    Update validation.c
    Update validation.c

--- validation.c
+++ validation.c
@@ -14,13 +14,51 @@ bool vali_date(dateStruct date)
     else return true;
 }
 
-char* validate_mail(char* email) {
-    for (char* ptr = email; *ptr; ptr++) {
-        if (*ptr == '@' && ptr != email) {
-            /// check that we saw at least one character
-            return ptr+1;
-        } else if (*ptr == ' ') { ///email should not have spaces in it
-            return 0;
+int validate_mail(char *s)
+{
+    int length=strlen(s), ind1=0, ind2=0, ind3=0; 
+ 
+ 
+    if ( (s[0]<'a') || (s[0]>'z') ) return 0;
+    else
+    {
+    for (int i=1;i<length;i++)
+    {
+        if (s[i]=='@')
+        {   
+            ind1=i+1;
+            break;
         }
+        else
+            if ( (s[i]<'a') || (s[i]>'z') ) return 0;
     }
-    return 0;
+ 
+ 
+    for (int j=ind1;j<length;j++)
+    {
+        if (s[j]=='.')
+        {   
+            ind2=j+1;
+            break;
+        }
+        else
+            if ( (s[j]<'a') || (s[j]>'z') ) return 0;
+    }
+ 
+ 
+    for (int k=ind2;k<length;k++)
+    {
+        if (s[k]=='.')
+        {   
+            ind3=k+1;
+            break;
+        }
+        else
+            if ( (s[k]<'a') || (s[k]>'z') ) return 0;
+    }
+    }
+ 
+ 
+    if (s[ind3] == 'i' && s[ind3+1] == 'l') return 1;
+    else return 0;
+}
