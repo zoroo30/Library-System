c6ae318e28bcd3e5a1e5178566ebd22e32b9af03
zoroo30 <Ahmed.zoroo30@outlook.com>
2017-12-08 07:44:29 +0200

    use int instead of bool in validation

--- borrowing.h
+++ borrowing.h
@@ -1,7 +1,14 @@
 typedef struct
 {
-    member whoBorrow;
-    book whatBorrow;
-    dateStruct whenBorrow;
-    dateStruct whenReturn;
-} borrow;
+    char book_isbn[10];
+    int member_id;
+    dateStruct date_issued;
+    dateStruct date_should_return;
+    dateStruct date_returned;
+} borrowing_record;
+
+borrowing_record br[100];
+
+void borrow(char book_isbn[10], int member_id){
+
+}
--- validation.h
+++ validation.h
@@ -1,6 +1,14 @@
 //validates negative input
-bool validateNegative(int n)
+int validateNegative(int n)
 {
-    if(n<0) return false;
-    else return true;
+    if(n<0) return 0;
+    else return 1;
+}
+
+int validate_date(int day,int month,int year)
+{
+    if((day>0&&day<=31) &&  (month>0&&month<=12) && (year>0))
+        return 1;
+    else
+        return 0;
 }
