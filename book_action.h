f49af1d7429cd24f4c7a50fcdb2e783bbd4b56e5
nour9821 <33841295+nour9821@users.noreply.github.com>
2017-12-17 12:51:42 +0200

    Update book_action.h

--- book_action.h
+++ book_action.h
@@ -1,5 +1,9 @@
+#ifndef BOOK_ACTION_H_INCLUDED
+#define BOOK_ACTION_H_INCLUDED
+
 typedef struct
 {
+
     int day;
     int month;
     int year;
@@ -7,14 +11,54 @@ typedef struct
 
 typedef struct
 {
+
+    int buildNum;//use validateNegative
+    char street[10];
+    char city[10];
+} addressStruct;
+
+typedef struct
+{
+
+    char name[10];
+    char domain[10];
+
+} emailStruct;
+
+typedef struct
+{
+
     char title[10];//we need to handle longer titles
     char author[10];//same
     char publisher[10];//same
-    char ISBN[20];
+    char ISBN[10];
     dateStruct date;
     int copies;
     int current;
+    int times_borrowed;
 } book;
+
+typedef struct
+{
+    char name[10];
+    int ID;
+    addressStruct address;
+    int mobile;
+    int age;
+    emailStruct email;
+    //add number of books borrowed field initialized to zero
+    int booksBorrowed;
+} member;
+typedef struct
+{
+
+    member whoBorrow;
+    book whatBorrow;
+    dateStruct whenBorrow;
+    dateStruct whenReturn;
+    dateStruct whenBack;
+} borrow;
+
 void insert_book(void);
 void show(book toShow);
 void search_title(char search[]);
@@ -23,3 +67,5 @@ void search_author(char search[]);
 void search_publisher(char search[]);
 void add_copy(char ISBN[],int* num_copies);
 void delete_book(char ISBN[]);
+
+#endif // BOOK_ACTION_H_INCLUDED
