90204d48c773eb50489d78dd4c3977b4f8df7b8f
nour9821 <33841295+nour9821@users.noreply.github.com>
2017-12-17 12:54:02 +0200

    Update members.h

--- members.h
+++ members.h
@@ -1,22 +1,8 @@
-typedef struct
-{
-    int buildNum;
-    char street[10];
-    char city[10];
-} addressStruct;
-typedef struct
-{
-    char name[10];
-    char domain[10];
+#ifndef MEMBER_H_INCLUDED
+#define MEMBER_H_INCLUDED
 
-} emailStruct;
-typedef struct
-{
-    char name[10];
-    int ID;
-    addressStruct address;
-    long int mobile;
-    int age;
-    emailStruct email;
-} member;
-int search_ID(int ID);
+int borrow_book(void);
+void registeration(void);
+int search_ID(int );
+
+#endif // MEMBER_H_INCLUDED
