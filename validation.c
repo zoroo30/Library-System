176b6ce294513013d8ff68ed1d6065647e5d8853
nour9821 <33841295+nour9821@users.noreply.github.com>
2017-12-17 12:52:36 +0200

    Update validation.c

--- validation.c
+++ validation.c
@@ -1,5 +1,11 @@
-# include<stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <string.h>
+#include <stdbool.h>
 #include "validation.h"
+#include "member.h"
+
 bool validateNegative(int n)
 {
     if(n<0) return false;
@@ -19,52 +25,9 @@ bool vali_date(char buffer[]){
       else return true;
   }
 }
-
-int validate_mail(char *s)
-{
-    int length=strlen(s), ind1=0, ind2=0, ind3=0; 
- 
- 
-    if ( (s[0]<'a') || (s[0]>'z') ) return 0;
-    else
-    {
-    for (int i=1;i<length;i++)
-    {
-        if (s[i]=='@')
-        {   
-            ind1=i+1;
-            break;
-        }
-        else
-            if ( (s[i]<'a') || (s[i]>'z') ) return 0;
-    }
- 
- 
-    for (int j=ind1;j<length;j++)
-    {
-        if (s[j]=='.')
-        {   
-            ind2=j+1;
-            break;
-        }
-        else
-            if ( (s[j]<'a') || (s[j]>'z') ) return 0;
-    }
- 
- 
-    for (int k=ind2;k<length;k++)
-    {
-        if (s[k]=='.')
-        {   
-            ind3=k+1;
-            break;
-        }
-        else
-            if ( (s[k]<'a') || (s[k]>'z') ) return 0;
-    }
-    }
- 
- 
-    if (s[ind3] == 'i' && s[ind3+1] == 'l') return 1;
-    else return 0;
+bool mobile(){
+int phone,start,end;
+scanf("  %n%d%n",&start,&phone,&end);
+if (end > 0 && (end-start)==11 && phone > 0) return true;
+else return false;
 }
