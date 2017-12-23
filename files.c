0d14772782f9e37c46384e394a0034f32e0a311c
nour9821 <33841295+nour9821@users.noreply.github.com>
2017-12-23 12:17:50 +0200

    Create files.c
    3 read from file
    3 write to file

---
+++ files.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "borrowing.h"
+#include "members.h"
+#include "book_action.h"
+#include "files.h"
+int write_books(book a[], int items, const char * fileName)
+{
+  FILE* ptr = fopen(fileName,"wb");
+  if(! ptr) return 0; // TODO: better error handling
+  fwrite( a, sizeof(book), items, ptr);
+  fclose(ptr);
+  return 1;
+}
+int read_books(book a[], const char * fileName)
+{
+  FILE* ptr = fopen(fileName,"rb");
+  if( !ptr ) return 0;
+  int n=0;
+ while(!feof(ptr))
+  {
+     fread(&a[n],sizeof(book),1,ptr);
+     n++;
+  }
+
+  fclose(ptr);
+  return 1 ;
+}
+
+int write_members(member a[], int items, const char * fileName)
+{
+  FILE* ptr = fopen(fileName,"wb");
+  if(! ptr) return 0; 
+  fwrite( a, sizeof(member), items, ptr);
+  fclose(ptr);
+  return 1;
+}
+int read_members(member a[], const char * fileName)
+{
+  FILE* ptr = fopen(fileName,"rb");
+  if( !ptr ) return 0;
+  int n=0;
+ while(!feof(ptr))
+  {
+     fread(&a[n],sizeof(member),1,ptr);
+     n++;
+  }
+
+  fclose(ptr);
+  return 1 ;
+}
+int write_borrowed(borrow a[], int items, const char * fileName)
+{
+  FILE* ptr = fopen(fileName,"wb");
+  if(! ptr) return 0; // TODO: better error handling
+  fwrite( a, sizeof(borrow), items, ptr);
+  fclose(ptr);
+  return 1;
+}
+int read_borrowed(borrow a[], const char * fileName)
+{
+  FILE* ptr = fopen(fileName,"rb");
+  if( !ptr ) return 0;
+  int n=0;
+ while(!feof(ptr))
+  {
+     fread(&a[n],sizeof(borrow),1,ptr);
+     n++;
+  }
+
+  fclose(ptr);
+  return 1 ;
+}
